57af4b0927dad411ef9cfca9f219f28efa74fdb1
Johnny Kaime <johnnyhou789@gmail.com>
2019-10-07 17:10:12 +0200

    Create bifid.py

---
+++ bifid.py
@@ -0,0 +1,19 @@
+def polybiusCipher(s): 
+	for char in s: 
+		# finding row of the table 
+		row = int((ord(char) - ord('a')) / 5) + 1
+		# finding column of the table  
+		col = ((ord(char) - ord('a')) % 5) + 1
+		# if character is 'k' 
+		if char == 'k': 
+			row = row - 1
+			col = 5 - col + 1
+		# if character is greater than 'j' 
+		elif ord(char) >= ord('j'): 
+			if col == 1 : 
+				col = 6
+				row = row - 1
+			col = col - 1                          
+		print(row, col, end ='', sep ='') 
+
+polybiusCipher("tf this is so bullshit")
